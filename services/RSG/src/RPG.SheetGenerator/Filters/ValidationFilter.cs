1d41f8b34499b57c48fa7fcd495a23fa50abc2e4
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-03-06 16:55:07 -0300

    feat: validation filter

---
+++ services/RSG/src/RPG.SheetGenerator/Filters/ValidationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RPG.SheetGenerator.API.Filters;
+public class ValidationFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errorMessages = context.ModelState
+                    .SelectMany(ms => ms.Value.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                context.Result = new BadRequestObjectResult(errorMessages);
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
