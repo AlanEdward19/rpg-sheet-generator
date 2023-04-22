42316326dc4f82c27101f25beca926877a2055e7
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:16:51 -0300

    feat: commandHandler

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Commands/Handler/CommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Commands.Handler;
+
+public class CommandHandler
+{
+    private readonly ICommandHandler _handler;
+
+    public CommandHandler(ICommandHandler handler)
+    {
+        _handler = handler;
+    }
+
+    public async Task Handle(ICommand command, int id) => await _handler.Update(command, id);
+    public async Task Handle(ICommand command, Guid id) => await _handler.Update(command, id);
+    public async Task Handle(ICommand command) => await _handler.Insert(command);
+    public async Task Handle(int id) => await _handler.Delete(id);
+}
