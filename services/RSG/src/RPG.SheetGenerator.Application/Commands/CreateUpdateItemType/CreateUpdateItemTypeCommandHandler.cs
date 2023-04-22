867f5e0094840337b7a5f6d1977517b1530b5508
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:14:13 -0300

    feat: itemType commands

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Commands/CreateUpdateItemType/CreateUpdateItemTypeCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+using RPG.SheetGenerator.Infrastructure.Context;
+
+namespace RPG.SheetGenerator.Application.Commands.CreateUpdateItemType;
+
+public class CreateUpdateItemTypeCommandHandler : ICommandHandler
+{
+    private readonly IRepository<ItemType> _repository;
+    private readonly DbSet<ItemType> _items;
+
+    public CreateUpdateItemTypeCommandHandler(IRepository<ItemType> repository, RSGDbContext dbContext)
+    {
+        _repository = repository;
+        _items = dbContext.Set<ItemType>();
+    }
+
+    public async Task Insert(ICommand command)
+    {
+        var model = command as CreateUpdateItemTypeCommand;
+
+        ItemType itemType = new()
+        {
+            Name = model.Name
+        };
+
+        await _repository.AddAsync(itemType);
+    }
+
+    public async Task Update(ICommand command, int id)
+    {
+        var model = command as CreateUpdateItemTypeCommand;
+
+        ItemType itemTypeDb = await _items.Where(x => x.Id.Equals(id)).FirstOrDefaultAsync();
+
+        itemTypeDb.Name = model.Name;
+
+        await _repository.UpdateAsync(itemTypeDb, id);
+    }
+
+    public Task Update(ICommand command, Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Delete(int id) => await _repository.DeleteById(id);
+
+    public Task Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
