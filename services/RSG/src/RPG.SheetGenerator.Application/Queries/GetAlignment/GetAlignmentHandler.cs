5969f634a228035ab3f0bfab492126f023c361a7
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:15:00 -0300

    feat: alignment get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetAlignment/GetAlignmentHandler.cs
@@ -0,0 +1,29 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetAlignment;
+
+public class GetAlignmentHandler : IQueryHandler<Alignment>
+{
+    private readonly IRepository<Alignment> _repository;
+
+    public GetAlignmentHandler(IRepository<Alignment> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Alignment>> GetAll()
+    {
+        return await _repository.GetAll();
+    }
+
+    public async Task<Alignment> GetById(int id)
+    {
+        return await _repository.GetById(id);
+    }
+
+    public Task<Alignment> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
