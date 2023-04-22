65a348ddd0b899fbe18e8c776614d04b651f748f
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:16:18 -0300

    feat: language get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetLanguage/GetLanguageHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetLanguage;
+
+public class GetLanguageHandler : IQueryHandler<Language>
+{
+    private readonly IRepository<Language> _repository;
+
+    public GetLanguageHandler(IRepository<Language> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Language>> GetAll() => await _repository.GetAll();
+    public async Task<Language> GetById(int id) => await _repository.GetById(id);
+
+    public Task<Language> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
