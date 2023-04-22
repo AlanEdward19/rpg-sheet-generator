f926c57459310dd86882c1e5f9c1703f8cd9c918
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:16:26 -0300

    feat: proficiency get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetProficiency/GetProficiencyHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetProficiency;
+
+public class GetProficiencyHandler : IQueryHandler<Proficiency>
+{
+    private readonly IRepository<Proficiency> _repository;
+
+    public GetProficiencyHandler(IRepository<Proficiency> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Proficiency>> GetAll() => await _repository.GetAll();
+    public async Task<Proficiency> GetById(int id) => await _repository.GetById(id);
+
+    public Task<Proficiency> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
