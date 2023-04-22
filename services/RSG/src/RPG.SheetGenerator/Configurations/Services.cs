51d9ed5edd6aee8b0ac8e97e610a85d5191d4133
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 16:52:28 -0300

    feat: dependency Injection

--- services/RSG/src/RPG.SheetGenerator/Configurations/Services.cs
+++ services/RSG/src/RPG.SheetGenerator/Configurations/Services.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RPG.SheetGenerator.Application.Commands.CreateAlignment;
+using RPG.SheetGenerator.Application.Queries.GetAlignment;
+using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
 using RPG.SheetGenerator.Infrastructure.Context;
+using RPG.SheetGenerator.Infrastructure.Repository;
+using Attribute = RPG.SheetGenerator.Core.Entities.Attribute;
 
 namespace BDS.TransferPrice.Configuration;
 
@@ -9,6 +15,23 @@ public static class Services
     {
         services.AddDbContext<RSGDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("Database")));
+
+        services.AddScoped<CreateUpdateAlignmentCommandHandler>();
+        services.AddScoped<GetAlignmentHandler>();
+
+        services.AddScoped<IRepository<Alignment>, Repository<Alignment>>();
+        services.AddScoped<IRepository<Attribute>, Repository<Attribute>>();
+        services.AddScoped<IRepository<Background>, Repository<Background>>();
+        services.AddScoped<IRepository<Campaign>, Repository<Campaign>>();
+        services.AddScoped<IRepository<Character>, Repository<Character>>();
+        services.AddScoped<IRepository<Class>, Repository<Class>>();
+        services.AddScoped<IRepository<Inventory>, Repository<Inventory>>();
+        services.AddScoped<IRepository<Item>, Repository<Item>>();
+        services.AddScoped<IRepository<ItemType>, Repository<ItemType>>();
+        services.AddScoped<IRepository<Language>, Repository<Language>>();
+        services.AddScoped<IRepository<Player>, Repository<Player>>();
+        services.AddScoped<IRepository<Proficiency>, Repository<Proficiency>>();
+        services.AddScoped<IRepository<Race>, Repository<Race>>();
 
         return services;
     }
