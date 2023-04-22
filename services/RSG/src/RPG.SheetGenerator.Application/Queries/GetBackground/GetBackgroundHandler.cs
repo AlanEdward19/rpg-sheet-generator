35afe9e1e851ffe62a6583bcc23e330219a48261
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:15:25 -0300

    feat: background get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetBackground/GetBackgroundHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetBackground;
+
+public class GetBackgroundHandler : IQueryHandler<Background>
+{
+    private readonly IRepository<Background> _repository;
+
+    public GetBackgroundHandler(IRepository<Background> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Background>> GetAll() => await _repository.GetAll();
+    public async Task<Background> GetById(int id) => await _repository.GetById(id);
+
+    public Task<Background> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
