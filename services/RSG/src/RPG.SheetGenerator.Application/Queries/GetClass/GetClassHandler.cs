97fc504d6673b0054493bc6174e7e9ba16ddc800
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:15:39 -0300

    feat: class get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetClass/GetClassHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetClass;
+
+public class GetClassHandler : IQueryHandler<Class>
+{
+    private readonly IRepository<Class> _repository;
+
+    public GetClassHandler(IRepository<Class> repository)
+    {
+        _repository = repository;
+    }
+    public async Task<List<Class>> GetAll() => await _repository.GetAll();
+
+    public async Task<Class> GetById(int id) => await _repository.GetById(id);
+
+    public Task<Class> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
