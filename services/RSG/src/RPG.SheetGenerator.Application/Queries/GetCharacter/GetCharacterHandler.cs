ed01c276a7cf383cef537eebf50d7ad6e20f3f60
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:15:33 -0300

    feat: character get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetCharacter/GetCharacterHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetCharacter;
+
+public class GetCharacterHandler : IQueryHandler<Character>
+{
+    private readonly IRepository<Character> _repository;
+
+    public GetCharacterHandler(IRepository<Character> repository)
+    {
+        _repository = repository;
+    }
+    public async Task<List<Character>> GetAll() => await _repository.GetAll();
+
+    public Task<Character> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Character> GetById(Guid id) => await _repository.GetById(id);
+}
