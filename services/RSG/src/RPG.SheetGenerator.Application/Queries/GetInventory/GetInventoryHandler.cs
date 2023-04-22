4120192d7f5b210e86f4c129390648e4e1767d1e
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:15:47 -0300

    feat: inventory get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetInventory/GetInventoryHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetInventory;
+
+public class GetInventoryHandler : IQueryHandler<Inventory>
+{
+    private readonly IRepository<Inventory> _repository;
+
+    public GetInventoryHandler(IRepository<Inventory> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Inventory>> GetAll() => await _repository.GetAll();
+    public async Task<Inventory> GetById(int id) => await _repository.GetById(id);
+    
+    public Task<Inventory> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
