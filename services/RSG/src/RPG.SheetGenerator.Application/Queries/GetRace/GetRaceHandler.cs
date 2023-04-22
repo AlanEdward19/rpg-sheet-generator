de4087e2fe416f390abf99b7c1cb9aa574a061bb
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:16:32 -0300

    feat: race get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetRace/GetRaceHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetRace;
+
+public class GetRaceHandler : IQueryHandler<Race>
+{
+    private readonly IRepository<Race> _repository;
+
+    public GetRaceHandler(IRepository<Race> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Race>> GetAll() => await _repository.GetAll();
+    public async Task<Race> GetById(int id) => await _repository.GetById(id);
+
+    public Task<Race> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
