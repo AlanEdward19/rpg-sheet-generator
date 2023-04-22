15f539ef412e287965fb3be247c1beaac3ca9891
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 16:53:07 -0300

    feat: alignment | attribute controller

--- services/RSG/src/RPG.SheetGenerator/Controllers/AlignmentController.cs
+++ services/RSG/src/RPG.SheetGenerator/Controllers/AlignmentController.cs
@@ -1,43 +1,38 @@
-﻿namespace RPG.SheetGenerator.API.Controllers;
+﻿using RPG.SheetGenerator.Application.Commands.CreateAlignment;
+using RPG.SheetGenerator.Application.Commands.Handler;
+using RPG.SheetGenerator.Application.Queries.GetAlignment;
+using RPG.SheetGenerator.Application.Queries.Handler;
+using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.API.Controllers;
 
 [ApiController]
 [ApiVersion("1.0")]
 [Route("[controller]/v{version:apiVersion}")]
 public class AlignmentController : ControllerBase
 {
+    private readonly CommandHandler _commandhandler;
+    private readonly QueryHandler<Alignment> _queryHandler;
 
-	public AlignmentController()
-	{
-		
-	}
+    public AlignmentController(CreateUpdateAlignmentCommandHandler commandHandler, GetAlignmentHandler queryHandler)
+    {
+        _commandhandler = new(commandHandler);
+        _queryHandler = new(queryHandler);
+    }
 
     [HttpGet("getAlignmentById")]
-    public async Task<IActionResult> Get(int id)
-    {
-        return Ok();
-    }
+    public async Task<IActionResult> Get(int id) => Ok(await _queryHandler.Handle(id));
 
     [HttpGet("getAlignments")]
-    public async Task<IActionResult> GetAll()
-    {
-        return Ok();
-    }
+    public async Task<IActionResult> GetAll() => Ok(await _queryHandler.Handle());
 
     [HttpPut("updateAlignment")]
-    public async Task Update()
-    {
-
-    }
+    public async Task Update([FromBody]CreateUpdateAlignmentCommand command, int id) => await _commandhandler.Handle(command, id);
 
     [HttpDelete("deleteAlignment")]
-    public async Task Delete()
-    {
-
-    }
+    public async Task Delete(int id) => await _commandhandler.Handle(id);
 
     [HttpPost("insertAlignment")]
-    public async Task Insert()
-    {
-
-    }
+    public async Task Insert([FromBody] CreateUpdateAlignmentCommand command) => await _commandhandler.Handle(command);
 }
--- services/RSG/src/RPG.SheetGenerator/Controllers/AttributeController.cs
+++ services/RSG/src/RPG.SheetGenerator/Controllers/AttributeController.cs
@@ -1,43 +1,37 @@
-﻿namespace RPG.SheetGenerator.API.Controllers;
+﻿using RPG.SheetGenerator.Application.Commands.CreateUpdateAttribute;
+using RPG.SheetGenerator.Application.Commands.Handler;
+using RPG.SheetGenerator.Application.Queries.GetAttribute;
+using RPG.SheetGenerator.Application.Queries.Handler;
+using Attribute = RPG.SheetGenerator.Core.Entities.Attribute;
+
+namespace RPG.SheetGenerator.API.Controllers;
 
 [ApiController]
 [ApiVersion("1.0")]
 [Route("[controller]/v{version:apiVersion}")]
 public class AttributeController : ControllerBase
 {
+    private readonly CommandHandler _commandhandler;
+    private readonly QueryHandler<Attribute> _queryHandler;
 
-	public AttributeController()
-	{
-		
-	}
+    public AttributeController(CreateUpdateAttributeCommandHandler commandHandler, GetAttributeHandler queryHandler)
+    {
+        _commandhandler = new(commandHandler);
+        _queryHandler = new(queryHandler);
+    }
 
     [HttpGet("getAttributeById")]
-    public async Task<IActionResult> Get(int id)
-    {
-        return Ok();
-    }
+    public async Task<IActionResult> Get(int id) => Ok(await _queryHandler.Handle(id));
 
     [HttpGet("getAttributes")]
-    public async Task<IActionResult> GetAll()
-    {
-        return Ok();
-    }
+    public async Task<IActionResult> GetAll() => Ok(await _queryHandler.Handle());
 
     [HttpPut("updateAttribute")]
-    public async Task Update()
-    {
-
-    }
+    public async Task Update([FromBody] CreateUpdateAttributeCommand command, int id) => await _commandhandler.Handle(command, id);
 
     [HttpDelete("deleteAttribute")]
-    public async Task Delete()
-    {
-
-    }
+    public async Task Delete(int id) => await _commandhandler.Handle(id);
 
     [HttpPost("insertAttribute")]
-    public async Task Insert()
-    {
-
-    }
+    public async Task Insert([FromBody] CreateUpdateAttributeCommand command) => await _commandhandler.Handle(command);
 }
