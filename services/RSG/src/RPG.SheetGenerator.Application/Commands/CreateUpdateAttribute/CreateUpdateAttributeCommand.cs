b1b5c17e672c6bad8ea541042efcf361f0a40005
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:13:00 -0300

    feat: attribute commands

--- services/RSG/src/RPG.SheetGenerator.Application/Commands/CreateUpdateAttribute/CreateUpdateAttributeCommand.cs
+++ services/RSG/src/RPG.SheetGenerator.Application/Commands/CreateUpdateAttribute/CreateUpdateAttributeCommand.cs
@@ -1,8 +1,12 @@
-﻿namespace RPG.SheetGenerator.Application.Commands.CreateUpdateAttribute;
+﻿using RPG.SheetGenerator.Core.Interfaces;
 
-public class CreateUpdateAttributeCommand
+namespace RPG.SheetGenerator.Application.Commands.CreateUpdateAttribute;
+
+public class CreateUpdateAttributeCommand : ICommand
 {
     public string Name { get; set; }
     public string Description { get; set; }
     public int Value { get; set; }
+
+    public IEnumerable<Guid>? CharacterIds { get; set; }
 }
---
+++ services/RSG/src/RPG.SheetGenerator.Application/Commands/CreateUpdateAttribute/CreateUpdateAttributeCommandHandler.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+using RPG.SheetGenerator.Infrastructure.Context;
+using Attribute = RPG.SheetGenerator.Core.Entities.Attribute;
+
+namespace RPG.SheetGenerator.Application.Commands.CreateUpdateAttribute;
+
+public class CreateUpdateAttributeCommandHandler : ICommandHandler
+{
+    private readonly IRepository<Attribute> _repository;
+    private readonly DbSet<Character> _dbSet;
+
+    public CreateUpdateAttributeCommandHandler(IRepository<Attribute> repository, RSGDbContext dbContext)
+    {
+        _repository = repository;
+        _dbSet = dbContext.Set<Character>();
+    }
+
+    public async Task Insert(ICommand command)
+    {
+        var model = command as CreateUpdateAttributeCommand;
+
+        var characterDb = model.CharacterIds is null
+            ? null
+            : await _dbSet.Where(x => model.CharacterIds.Contains(x.Id)).ToListAsync();
+
+        Attribute attribute = new()
+        {
+            Name = model.Name,
+            Description = model.Description,
+            Value = model.Value,
+            Characters = characterDb
+        };
+
+        await _repository.AddAsync(attribute);
+    }
+
+    public async Task Update(ICommand command, int id)
+    {
+        var model = command as CreateUpdateAttributeCommand;
+
+        var characterDb = model.CharacterIds is null
+            ? null
+            : await _dbSet.Where(x => model.CharacterIds.Contains(x.Id)).ToListAsync();
+
+        Attribute attribute = new()
+        {
+            Name = model.Name,
+            Description = model.Description,
+            Value = model.Value,
+            Characters = characterDb
+        };
+
+        await _repository.UpdateAsync(attribute, id);
+    }
+
+    public async Task Delete(int id) => await _repository.DeleteById(id);
+
+    public Task Update(ICommand command, Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
