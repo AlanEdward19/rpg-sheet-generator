14d19cc245d5a011fd94223c57fe96353818fc4e
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:13:09 -0300

    feat: alignment commands

--- services/RSG/src/RPG.SheetGenerator.Application/Commands/CreateUpdateAlignment/CreateUpdateAlignmentCommand.cs
+++ services/RSG/src/RPG.SheetGenerator.Application/Commands/CreateUpdateAlignment/CreateUpdateAlignmentCommand.cs
@@ -1,6 +1,8 @@
-﻿namespace RPG.SheetGenerator.Application.Commands.CreateAlignment;
+﻿using RPG.SheetGenerator.Core.Interfaces;
 
-public class CreateUpdateAlignmentCommand
+namespace RPG.SheetGenerator.Application.Commands.CreateAlignment;
+
+public class CreateUpdateAlignmentCommand : ICommand
 {
     public string Name { get; set; }
     public string Description { get; set; }
---
+++ services/RSG/src/RPG.SheetGenerator.Application/Commands/CreateUpdateAlignment/CreateUpdateAlignmentCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+using RPG.SheetGenerator.Infrastructure.Context;
+
+namespace RPG.SheetGenerator.Application.Commands.CreateAlignment;
+
+public class CreateUpdateAlignmentCommandHandler : ICommandHandler
+{
+    private readonly IRepository<Alignment> _repository;
+
+    public CreateUpdateAlignmentCommandHandler(IRepository<Alignment> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Insert(ICommand command)
+    {
+        var model = command as CreateUpdateAlignmentCommand;
+
+        Alignment alignment = new()
+        {
+            Description = model.Description,
+            Name = model.Name
+        };
+
+        await _repository.AddAsync(alignment);
+    }
+
+    public async Task Update(ICommand command, int id)
+    {
+        var model = command as CreateUpdateAlignmentCommand;
+
+        Alignment alignment = new()
+        {
+            Description = model.Description,
+            Name = model.Name
+        };
+
+        await _repository.UpdateAsync(alignment, id);
+    }
+
+    public async Task Delete(int id) => await _repository.DeleteById(id);
+
+    public Task Update(ICommand command, Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
