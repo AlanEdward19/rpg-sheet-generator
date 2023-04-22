ee8b5be95d4548e621f618b4ac785345a083e1b1
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:18:06 -0300

    feat: services

--- services/RSG/src/RPG.SheetGenerator/Configurations/Services.cs
+++ services/RSG/src/RPG.SheetGenerator/Configurations/Services.cs
@@ -1,6 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RPG.SheetGenerator.Application.Commands.CreateAlignment;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateAttribute;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateBackground;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateCharacter;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateClass;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateInventory;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateItem;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateItemType;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateLanguage;
+using RPG.SheetGenerator.Application.Commands.CreateUpdatePlayer;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateProficiency;
+using RPG.SheetGenerator.Application.Commands.CreateUpdateRace;
 using RPG.SheetGenerator.Application.Queries.GetAlignment;
+using RPG.SheetGenerator.Application.Queries.GetAttribute;
+using RPG.SheetGenerator.Application.Queries.GetBackground;
+using RPG.SheetGenerator.Application.Queries.GetCharacter;
+using RPG.SheetGenerator.Application.Queries.GetClass;
+using RPG.SheetGenerator.Application.Queries.GetInventory;
+using RPG.SheetGenerator.Application.Queries.GetItem;
+using RPG.SheetGenerator.Application.Queries.GetItemType;
+using RPG.SheetGenerator.Application.Queries.GetLanguage;
+using RPG.SheetGenerator.Application.Queries.GetPlayer;
+using RPG.SheetGenerator.Application.Queries.GetProficiency;
+using RPG.SheetGenerator.Application.Queries.GetRace;
 using RPG.SheetGenerator.Core.Entities;
 using RPG.SheetGenerator.Core.Interfaces;
 using RPG.SheetGenerator.Infrastructure.Context;
@@ -16,8 +38,39 @@ public static class Services
         services.AddDbContext<RSGDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("Database")));
 
+        #region Commands
+
         services.AddScoped<CreateUpdateAlignmentCommandHandler>();
+        services.AddScoped<CreateUpdateAttributeCommandHandler>();
+        services.AddScoped<CreateUpdateBackgroundCommandHandler>();
+        services.AddScoped<CreateUpdateCharacterCommandHandler>();
+        services.AddScoped<CreateUpdateClassCommandHandler>();
+        services.AddScoped<CreateUpdateInventoryCommandHandler>();
+        services.AddScoped<CreateUpdateItemCommandHandler>();
+        services.AddScoped<CreateUpdateItemTypeCommandHandler>();
+        services.AddScoped<CreateUpdateLanguageCommandHandler>();
+        services.AddScoped<CreateUpdatePlayerCommandHandler>();
+        services.AddScoped<CreateUpdateProficiencyCommandHandler>();
+        services.AddScoped<CreateUpdateRaceCommandHandler>();
+
+        #endregion
+
+        #region Queries
+
         services.AddScoped<GetAlignmentHandler>();
+        services.AddScoped<GetAttributeHandler>();
+        services.AddScoped<GetBackgroundHandler>();
+        services.AddScoped<GetCharacterHandler>();
+        services.AddScoped<GetClassHandler>();
+        services.AddScoped<GetInventoryHandler>();
+        services.AddScoped<GetItemHandler>();
+        services.AddScoped<GetItemTypeHandler>();
+        services.AddScoped<GetLanguageHandler>();
+        services.AddScoped<GetPlayerHandler>();
+        services.AddScoped<GetProficiencyHandler>();
+        services.AddScoped<GetRaceHandler>();
+
+        #endregion
 
         services.AddScoped<IRepository<Alignment>, Repository<Alignment>>();
         services.AddScoped<IRepository<Attribute>, Repository<Attribute>>();
