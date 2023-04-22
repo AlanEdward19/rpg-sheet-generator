054a7a9c497050a04914ca2887d9dfc5a331d7c8
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:16:11 -0300

    feat: player get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetPlayer/GetPlayerHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetPlayer;
+
+public class GetPlayerHandler : IQueryHandler<Player>
+{
+    private readonly IRepository<Player> _repository;
+
+    public GetPlayerHandler(IRepository<Player> repository)
+    {
+        _repository = repository;
+    }
+    public async Task<List<Player>> GetAll() => await _repository.GetAll();
+
+    public Task<Player> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Player> GetById(Guid id) => await _repository.GetById(id);
+}
