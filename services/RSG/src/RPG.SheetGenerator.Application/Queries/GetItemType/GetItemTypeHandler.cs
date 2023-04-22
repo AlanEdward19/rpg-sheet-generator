bee468a6c676048f3752eb0c22c7af46bd405185
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:16:01 -0300

    feat: itemType get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetItemType/GetItemTypeHandler.cs
@@ -0,0 +1,22 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+
+namespace RPG.SheetGenerator.Application.Queries.GetItemType;
+
+public class GetItemTypeHandler : IQueryHandler<ItemType>
+{
+    private readonly IRepository<ItemType> _repository;
+
+    public GetItemTypeHandler(IRepository<ItemType> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<ItemType>> GetAll() => await _repository.GetAll();
+    public async Task<ItemType> GetById(int id) => await _repository.GetById(id);
+
+    public Task<ItemType> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
