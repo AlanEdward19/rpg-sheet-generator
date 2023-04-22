a7f96cbae0ecfb7a718b04c4128025deae2335be
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-22 18:15:13 -0300

    feat: attribute get

---
+++ services/RSG/src/RPG.SheetGenerator.Application/Queries/GetAttribute/GetAttributeHandler.cs
@@ -0,0 +1,29 @@
+﻿using RPG.SheetGenerator.Core.Entities;
+using RPG.SheetGenerator.Core.Interfaces;
+using Attribute = RPG.SheetGenerator.Core.Entities.Attribute;
+
+namespace RPG.SheetGenerator.Application.Queries.GetAttribute;
+
+public class GetAttributeHandler : IQueryHandler<Attribute>
+{
+    private readonly IRepository<Attribute> _repository;
+
+    public GetAttributeHandler(IRepository<Attribute> repository)
+    {
+        _repository = repository;
+    }
+    public async Task<List<Attribute>> GetAll()
+    {
+        return await _repository.GetAll();
+    }
+
+    public async Task<Attribute> GetById(int id)
+    {
+        return await _repository.GetById(id);
+    }
+
+    public Task<Attribute> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
